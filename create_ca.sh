1a8ceac58eccb37dfa85ab8f23e3a43393b4b23d
metamic <meta.microcode@gmail.com>
2020-12-23 19:55:11 +0900

    fix create_ca.sh

--- create_ca.sh
+++ create_ca.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+if [ ! -d cert ]; then
+  mkdir cert
+fi
+
 openssl genrsa -out cert/ca.key 2048
 openssl req -new -key cert/ca.key -out cert/ca.csr
 openssl x509 -req -days 3650 -extensions v3_ca -set_serial 1 -in cert/ca.csr -signkey cert/ca.key -out cert/ca.crt
